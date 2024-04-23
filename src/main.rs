833f47265e052fca8eba62d24002bf0c1572b479
CowSociety <ryan.avery.spaker@protonmail.com>
2024-04-22 20:09:25 -0400

    Added a*

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod graph;
 fn main() {
-    let graph = graph::Graph::new_california();
-    println!("{} {} {} {}", graph.avg_edge_count(), graph.shortest_path_bfs(0, 1000), graph.shortest_path_bfs_heuristic(0, 1000), graph.shortest_path_dijkstras(0, 1000));
+    let graph = graph::Graph::new_random(2000, 4000);
+    println!("{} {} {} {} {}", graph.avg_edge_count(), graph.shortest_path_bfs(0, 1000), graph.shortest_path_bfs_heuristic(0, 1000), graph.shortest_path_dijkstras(0, 1000), graph.shortest_path_a_star(0, 1000));
 }
